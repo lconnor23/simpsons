454e58981e8344dbe541b85eaebdb2a8bc1ec59d
Laura Connor <itslauraroseco@gmail.com>
2023-01-23 12:36:34 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// //Example fetch using pokemonapi.co
+document.querySelector('#random').addEventListener('click', getRandom)
+// document.querySelector('#character').addEventListener('click', getCharacter)
+
+function getRandom(){
+  const inputCharacter = document.querySelector('input').value
+  const url = `https://thesimpsonsquoteapi.glitch.me/quotes?character=${inputCharacter}`
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    document.querySelector('h2').innerText = data[0].character
+    document.querySelector('#characterImg').src = data[0].image
+    document.querySelector('h3').innerText = data[0].quote
+  })
+}
